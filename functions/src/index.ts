1afe3d6b4069feec98af711e3efbf4585d442084
arsalan.ahmed1264@gmail.com <arsalan.ahmed1264@gmail.com>
2019-08-23 02:57:41 +0500

    working with databse

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,10 +7,13 @@
 //
 
 // import the appropriate class
-const functions = require('firebase-functions')
+const functions = require('firebase-functions');
+const admin = require("firebase-admin");
 const { WebhookClient } = require('dialogflow-fulfillment');
 const { Suggestion } = require("dialogflow-fulfillment");
 
+admin.initializeApp(functions.config().firebase);
+const firestore = admin.firestore();
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
     (request, response) => {
@@ -35,36 +38,87 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
 
 
         function usb(agent) {
-            
-            if(agent.parameters.storage) {
-                const storage_usb:number = agent.parameters.storage;    
-            }else {
-                agent.add(`Select storage for usb, we've `);
+
+            const storage_usb: number = agent.parameters.storage;
+
+            const brand: string = agent.parameters.brand;
+
+            const price: number = agent.parameters.price.amount;
+
+            const port: number = agent.parameters.port;
+
+
+            if (storage_usb) {
+                // do nothing if true
+            } else {
+                agent.add('Select storage, we have ')
                 agent.add(new Suggestion('16 GB'));
                 agent.add(new Suggestion('32 GB'));
                 agent.add(new Suggestion('64 GB'));
             }
-            
-            if (agent.parameters.brand) {
-                const brand:string = agent.parameters.brand;
-                
-            }else {
-                agent.add(`Would you like any specific brand, we've `);
-                agent.add(new Suggestion('Samsung'));
-                agent.add(new Suggestion('Kingston'));
+
+            if (storage_usb) {
+                if (price) {
+                    // do nothing if true
+                } else {
+                    agent.add('Enter your expected price');
+                }
             }
 
-            if(agent.parameters.price) {
-                let price:number = agent.parameters.price
-            }else {
-                agent.add(`Your budget?`);
+            if (storage_usb && price) {
+                agent.add(`storage ${storage_usb} ,brand ${brand} ,price ${price} and port ${port}`);
+                let result24 = show_products(storage_usb, price, brand, price);
             }
-
-             const port:number = agent.parameters.port;
-
-            agent.add(`storage ${storage_usb} ,brand ${brand} ,price ${price} and port ${port}` );
 
         }
+
+
+        function add_products(agent) {
+            const params = agent.parameters;
+            firestore.collection("usb").add(params)
+                .then(() => {
+                    response.send({
+                        fulfillmentText:
+                            `${params.brand} usb, ${params.price} price, ${params.port} port, storage ${params.storage} added to database.`
+                    });
+
+                    return response.status(200);
+                })
+                .catch((e) => {
+                    response.send({
+                        fulfillmentText: "Something went wrong when writing in database."
+                    });
+                });
+        }
+
+        function show_products(usb_storage: Number, usb_ports: Number, usb_brand: String, usb_price: Number): void {
+            firestore.collection('usb').get()
+            .then((querySnapshot) => {
+
+                const orders = [];
+                querySnapshot.forEach((doc) => { orders.push(doc.data()) });
+
+                let speech = `Here is usb \n`;
+
+                orders.forEach((eachOrder, index) => {
+                    speech += `\n Brand ${index + 1} is ${eachOrder.brand} brand for ${eachOrder.price} price, ordered by ${eachOrder.storage} GB and ${eachOrder.port} port \n`
+                })
+
+                response.send({
+                    fulfillmentText: speech
+                });
+                return response.status(200);
+            })
+            .catch((err) => {
+                console.log('Error getting documents', err);
+
+                response.send({
+                    speech: "something went wrong when reading from database"
+                })
+            })
+
+        }
+
 
 
         // Run the proper handler based on the matched Dialogflow intent
@@ -74,9 +128,11 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
         intentMap.set("Default Welcome Intent", welcome);
         intentMap.set("Default Fallback Intent", fallback);
         intentMap.set("usb", usb);
+        intentMap.set("add products", add_products);
 
         //intentMap.set("room pictures", show_images);
         _agent.handleRequest(intentMap);
 
     }
 );
+
